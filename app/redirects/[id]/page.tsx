56ce9abd92b5555dc058659fc4bc756083defbdc
Leonardo Crociani <l.crociani@outlook.com>
2025-04-28 19:09:34 +0200

    sexier redirects :)

---
+++ app/redirects/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { redirectsMap } from "@/constants/redirects_map";
+import { redirect } from "next/navigation";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const { id } = await params;
+
+    const redirectUrl = redirectsMap[id];
+    if (redirectUrl) {
+        redirect(redirectUrl);
+    } else {
+        // Handle the case where the ID is not found in the redirectsMap
+        // You can redirect to a 404 page or show an error message
+        redirect("/404");
+    }
+    return null;
+}
