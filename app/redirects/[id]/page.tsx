c36bc6802b9969323a699d6d9a17ea863009aa7a
Leonardo Crociani <l.crociani@outlook.com>
2025-04-28 19:11:25 +0200

    type fix

--- app/redirects/[id]/page.tsx
+++ app/redirects/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { redirectsMap } from "@/constants/redirects_map";
 import { redirect } from "next/navigation";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
     const redirectUrl = redirectsMap[id];
