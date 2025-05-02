075d7f951359be795348758d3bc8d24f411565a8
Leonardo Crociani <l.crociani@outlook.com>
2025-05-02 09:56:37 +0200

    done

--- constants/redirects_map.ts
+++ constants/redirects_map.ts
@@ -1,3 +1,3 @@
 export const redirectsMap: Record<string, string> = {
-    "syndrive" : "https://drive.google.com/file/d/1juxMcpqFL_sIdsMZ6dj0_lLWO9bUnkiN/view?usp=sharing"
+    "syndrive" : "https://drive.google.com/file/d/1PNSH2y4D6zrNaQQPoNbZe2J0PuXevS1M/view?usp=sharing"
 }
