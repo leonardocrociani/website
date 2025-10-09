41c41c093ce42e9aa1990c2b4b524d4b3d17094e
Leonardo Crociani <l.crociani@outlook.com>
2025-10-09 10:34:29 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,6 @@ export const revalidate = 0;
 export default async function Page() {
 
     await kv.increment("home-page-views", 1);
-
-    await kv.set("citations", [
-        {
-            citation: 'What sets successful cases apart from failures is that the entrepreneurs who make it have the foresight, skills, and tools to identify which parts of their plan are working exceptionally well and which are flawed — and to adapt their strategy accordingly.',
-            source: 'The Lean Startup, Eric Ries'
-        },
-        {
-            citation : 'Your network is your net worth.',
-            source: 'Porter Gale, author of Your Network is Your Net Worth'
-        }
-    ]);
-
     const books = await kv.get("books", []) as { title: string; author: string; link: string }[];
     const citations = await kv.get("citations", []) as { citation: string; source: string }[];
 
